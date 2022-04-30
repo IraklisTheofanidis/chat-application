0a6ced797fa2c7c4c6204c95c80628ecc41fb9ed
Hercules <56442488+it164664@users.noreply.github.com>
2022-04-30 12:21:59 +0300

    add angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,10 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from '../environments/environment';
 import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -38,7 +42,11 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAnalyticsModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: false,
-  firebase:
-  {
-      apiKey: "AIzaSyAACeqiPKfZpJ1XkBZHKTVI303hpfxH2UE",
-      authDomain: "chat-23ed2.firebaseapp.com",
-      projectId: "chat-23ed2",
-      storageBucket: "chat-23ed2.appspot.com",
-      messagingSenderId: "851813110942",
-      appId: "1:851813110942:web:5ab902cdaa834a4cda178f",
-      measurementId: "G-5TZBS1NNVS"
+  firebase: {
+    projectId: 'chat-23ed2',
+    appId: '1:851813110942:web:5ab902cdaa834a4cda178f',
+    databaseURL: 'https://chat-23ed2-default-rtdb.firebaseio.com',
+    storageBucket: 'chat-23ed2.appspot.com',
+    apiKey: 'AIzaSyAACeqiPKfZpJ1XkBZHKTVI303hpfxH2UE',
+    authDomain: 'chat-23ed2.firebaseapp.com',
+    messagingSenderId: '851813110942',
+    measurementId: 'G-5TZBS1NNVS',
   }
 };
 
