efe34c5a3dcd36458f84366eed444fbd09ee7aad
Hercules <56442488+it164664@users.noreply.github.com>
2022-09-24 20:20:07 +0300

    adds angular/fire

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'chat-23ed2',
+    appId: '1:851813110942:web:5ab902cdaa834a4cda178f',
+    databaseURL: 'https://chat-23ed2-default-rtdb.firebaseio.com',
+    storageBucket: 'chat-23ed2.appspot.com',
+    apiKey: 'AIzaSyAACeqiPKfZpJ1XkBZHKTVI303hpfxH2UE',
+    authDomain: 'chat-23ed2.firebaseapp.com',
+    messagingSenderId: '851813110942',
+    measurementId: 'G-5TZBS1NNVS',
+  },
   production: false
 };
 
